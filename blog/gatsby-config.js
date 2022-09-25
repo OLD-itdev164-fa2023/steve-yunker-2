557994d5565c4250b3ee520531ee353980f73268
steve yunker <yunkers@gmatc.matc.edu>
2022-09-25 13:02:01 -0500

    Completed Activity 1

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -1,9 +1,14 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `ITDEV-164 Gatsby Blog`,
+    author: `Steve Yunker`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    contact: {
+      name: `Steve Yunker`,
+      company: `Blogs Inc`,
+      address: `PO Box 1234`
+    }
   },
   plugins: [
     `gatsby-plugin-image`,
