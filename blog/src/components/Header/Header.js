1ff944325ea6e8afcc22c5726244d7cc0ec80086
steve yunker <yunkers@gmatc.matc.edu>
2022-10-21 20:57:55 -0500

    Completed activity 6

--- blog/src/components/Header/Header.js
+++ blog/src/components/Header/Header.js
@@ -1,21 +1,46 @@
-const { propTypes } = require("gatsby-plugin-image/dist/src/components/gatsby-image.server")
+import * as React from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
+import styled from 'styled-components'
+
+const Outer = styled.header`
+  background: ${props => props.theme.header.backgroundColor};
+  margin-bottom: 1.45rem;
+  margin: 0 auto;
+  padding: var(--space-4) var(--size-gutter);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const H1 = styled.h1`
+  margin: 0px;
+`
+
+const StyledLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+  &:hover {
+    color: lightgray;
+  }
+`
 
 const Header = ({ siteTitle }) => (
-<Outer>
+  <Outer>
     <H1>
-        <StyledLink to="/">
-            {siteTitle}
-        </StyledLink>
+      <StyledLink to="/">
+        {siteTitle}
+      </StyledLink>
     </H1>
-</Outer>
+  </Outer>
 )
 
 Header.propTypes = {
-    siteTitle: propTypes.string,
+  siteTitle: PropTypes.string,
 }
 
 Header.defaultProps = {
-    siteTitle: ``,
+  siteTitle: ``,
 }
 
 export { Header }
